ecea9408bc0da6e64ee2c8504f53508ba38a2572
sunnyheee <fool0116@naver.com>
2023-11-23 22:13:06 +0900

    start project

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className=" bg-slate-900 text-sm text-white py-2">
+        {"Don't forget to CODE your DREAM | All Right Reserved."}
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
