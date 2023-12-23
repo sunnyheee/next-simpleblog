1ac83784282e9452b4af423d1c0172b4199c90ce
sunnyheee <fool0116@naver.com>
2023-12-23 20:52:55 +0900

    category selected

---
+++ src/app/_components/FilteralblePosts.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Post } from "@/api/posts";
+import { useState } from "react";
+import PostsGrid from "./PostsGrid";
+import Categories from "./Categories";
+
+type Props = {
+  posts: Post[];
+  categories: string[];
+};
+const ALL_POSTS = "All_Posts";
+
+export default function FilteralblePosts({ posts, categories }: Props) {
+  const [selected, setSelected] = useState(ALL_POSTS);
+  const filtered =
+    selected === ALL_POSTS
+      ? posts
+      : posts.filter((post) => post.category === selected);
+  return (
+    <section className="flex m-4">
+      <PostsGrid posts={filtered} />
+      <Categories
+        categories={[ALL_POSTS, ...categories]}
+        selected={selected}
+        onClick={setSelected}
+      />
+    </section>
+  );
+}
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import PostsGrid from "../_components/PostsGrid";
+import { getAllPosts } from "@/api/posts";
+import FilteralblePosts from "../_components/FilteralblePosts";
 
-const Posts = () => {
-  return <div>posts</div>;
+const PostsPage = async () => {
+  const posts = await getAllPosts();
+
+  // Set은 중복일 경우 버려줌
+  // category를 찾아서 categories 배열에 넣어줌
+  const categories = [...new Set(posts.map((post) => post.category))];
+  console.log(categories);
+
+  return <FilteralblePosts posts={posts} categories={categories} />;
 };
 
-export default Posts;
+export default PostsPage;
