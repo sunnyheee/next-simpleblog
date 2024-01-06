9cb893ec7183ff8a9b6570d60a9a4a2dc4ce5549
sunnyheee <fool0116@naver.com>
2024-01-06 16:58:39 +0900

    create contact, about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Hero from "../_components/Hero";
 
+const TITLE_CLASS = " text-2xl font-bold text-gray-800 my2";
 const About = () => {
-  return <div>about</div>;
+  return (
+    <>
+      <Hero />
+      <section className=" bg-gray-100 shadow-lg p-8 m-8 text-center">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          개발을 사랑하는 프론트 개발자
+          <br />
+          사람과 디자인을 담는 웹앱을 만들고 싶음
+        </p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>Bizlink (-Now)</p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>React, jQuery, Vue</p>
+      </section>
+    </>
+  );
 };
 
 export default About;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,35 @@
+import { link } from "fs";
 import React from "react";
-
+import { AiFillGithub, AiFillInstagram, AiFillYoutube } from "react-icons/ai";
+import ContactFrom from "../_components/ContactFrom";
+const LINKS = [
+  { icon: <AiFillGithub />, url: "" },
+  { icon: <AiFillInstagram />, url: "" },
+  { icon: <AiFillYoutube />, url: "" },
+];
 const Contact = () => {
-  throw new Error();
-  return <div>Contact</div>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>sunnyheee0@gmail.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, index) => (
+          <li key={index}>
+            <a
+              href={link.url}
+              target="_blank"
+              rel="noreferrer"
+              className="text-5xl hover:text-yellow-400"
+            >
+              {link.icon}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <h2 className=" text-3xl font-bold my-8 ">Or Send me an email</h2>
+      <ContactFrom />
+    </section>
+  );
 };
 
 export default Contact;
