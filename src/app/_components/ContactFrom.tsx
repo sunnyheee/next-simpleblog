aaa17b0f361012698f544210ef24a1c1c839a11e
sunnyheee <fool0116@naver.com>
2024-01-10 22:40:15 +0900

    add contact page

--- src/app/_components/ContactFrom.tsx
+++ src/app/_components/ContactFrom.tsx
@@ -1,7 +1,81 @@
-import React from "react";
+"use client";
+import { ChangeEvent, FormEvent, useState } from "react";
+import Banner, { BannerData } from "./Banner";
+
+type Form = {
+  from: string;
+  subject: string;
+  message: string;
+};
 
 const ContactFrom = () => {
-  return <div>ContactFrom</div>;
+  const [form, setForm] = useState<Form>({
+    from: "",
+    subject: "",
+    message: "",
+  });
+  const [banner, setBanner] = useState<BannerData | null>(null);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
+    setBanner({ message: "성공", state: "success" });
+    setTimeout(() => {
+      setBanner(null);
+    }, 3000);
+  };
+  return (
+    <section className="w-full max-w-md ">
+      {banner && <Banner banner={banner} />}
+      <form
+        onSubmit={onSubmit}
+        className="w-full flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-lg text-white"
+      >
+        <label htmlFor="from" className="font-semibold">
+          Your Email
+        </label>
+        <input
+          type="email"
+          id="from"
+          name="from"
+          required
+          autoFocus
+          value={form.from}
+          onChange={onChange}
+        />
+        <label htmlFor="subject" className="font-semibold">
+          Subject
+        </label>
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          required
+          value={form.subject}
+          onChange={onChange}
+        />
+        <label htmlFor="message" className="font-semibold">
+          Message
+        </label>
+        <textarea
+          className="text-black"
+          rows={10}
+          id="message"
+          name="message"
+          required
+          value={form.message}
+          onChange={onChange}
+        />
+        <button className="py-2 bg-yellow-300 text-black font-bold hover:bg-yellow-400">
+          submit
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default ContactFrom;
